5d05c9c01619d934d3430a38a363c0e88ce8710f
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-09-16 22:05:58 -0300

    Update biblioteca_git.py

--- biblioteca_git.py
+++ biblioteca_git.py
@@ -427,7 +427,7 @@ def generar_html(pdfs):
     <script>
     if ('serviceWorker' in navigator) {{
       window.addEventListener('load', function() {{
-        navigator.serviceWorker.register("static/service-worker.js").then(function(registration) {{
+        navigator.serviceWorker.register("static/service-worker.js", {{ scope: "/"}}).then(function(registration) {{
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }}, function(err) {{
           console.log('ServiceWorker registration failed: ', err);
