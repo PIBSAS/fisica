5a738cc573edd837b1e516c6ff58990a27b4be42
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-09-16 22:13:44 -0300

    Update biblioteca_git.py

--- biblioteca_git.py
+++ biblioteca_git.py
@@ -165,7 +165,7 @@ def crear_manifest():
 
 def crear_service_worker(pdfs):
     """Crea el service-worker.js para caché de la PWA."""
-    urls = ["./", "logo.webp", "logo_pwa.png", "logo_pwa-192.png", "logo_pwa-512.png", "logo_pwa-1024.png", "favicon.ico", "site.webmanifest", "pdfjs/web/viewer.html", "pdfjs/build/pdf.js", "pdfjs/build/pdf.worker.js"]
+    urls = ["./", "static/logo.webp", "static/logo_pwa.png", "static/logo_pwa-192.png", "static/logo_pwa-512.png", "static/logo_pwa-1024.png", "static/favicon.ico", "static/site.webmanifest", "static/pdfjs/web/viewer.html", "static/pdfjs/build/pdf.js", "static/pdfjs/build/pdf.worker.js"]
     
     for _, _, archivo in pdfs:
         base = os.path.splitext(archivo)[0]
@@ -427,7 +427,7 @@ def generar_html(pdfs):
     <script>
     if ('serviceWorker' in navigator) {{
       window.addEventListener('load', function() {{
-        navigator.serviceWorker.register("static/service-worker.js", {{ scope: "/"}}).then(function(registration) {{
+        navigator.serviceWorker.register("service-worker.js").then(function(registration) {{
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }}, function(err) {{
           console.log('ServiceWorker registration failed: ', err);
